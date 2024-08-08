9541fa16c565fea0851e8dc659a2393a93ae1386
Juan Torres <jdtorreserazo@gmail.com>
2024-08-08 12:10:42 -0700

    FIXED CORS

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ const JWT_SECRET_REFRESH = process.env.JWT_SECRET_REFRESH;
 const allowedOrigins = [
     "https://intacsep.spotynet.com", // Production
     "http://localhost:5173", // Development,
-    "https://intacsep-client-8z0b.onrender.com",
+    "https://intacsep-client.onrender.com", //Pruebas
 ];
 
 app.use(
